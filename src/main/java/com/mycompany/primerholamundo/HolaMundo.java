ee3a92ffd23d99dbb4c9727345ee34e751f70278
153patri <losalgarrobos109@gmail.com>
2022-11-21 12:38:44 -0300

    probando Java

---
+++ src/main/java/com/mycompany/primerholamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+package com.mycompany.primerholamundo;
+
+/**
+ *
+ * @author Patricia
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/primerholamundo/PrimerHolaMundo.java
@@ -0,0 +1,10 @@
+package com.mycompany.primerholamundo;
+
+
+public class PrimerHolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("***Fin del programa***");
+    }
+}
